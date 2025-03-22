8e30854295841b09db645da639f0a431bd96e8d2
User <user@example.com>
2025-03-22 22:57:34 +0700

    sa

---
+++ src/lib/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBnesMhd69L5Gjkr6nAy7rpy4GLrQIH0e8",
+  authDomain: "ktcpv-d48b8.firebaseapp.com",
+  projectId: "ktcpv-d48b8",
+  storageBucket: "ktcpv-d48b8.firebasestorage.app",
+  messagingSenderId: "620942377287",
+  appId: "1:620942377287:web:81fe0396037e1c25950c62"
+}; 
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,15 +8,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword
 } from "firebase/auth";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBnesMhd69L5Gjkr6nAy7rpy4GLrQIH0e8",
-  authDomain: "ktcpv-d48b8.firebaseapp.com",
-  projectId: "ktcpv-d48b8",
-  storageBucket: "ktcpv-d48b8.firebasestorage.app",
-  messagingSenderId: "620942377287",
-  appId: "1:620942377287:web:81fe0396037e1c25950c62"
-};
+import { firebaseConfig } from "./firebase.config";
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
